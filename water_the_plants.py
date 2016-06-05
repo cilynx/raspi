14559c2c2fb605c5365d45b41af9ce1abf134c7c
Randy C. Will <randall.will@gmail.com>
2016-06-04 20:20:08 -0700

    Get rid of counter.  Assume first "dry" is reliable.

--- water_the_plants.py
+++ water_the_plants.py
@@ -1,39 +1,44 @@
-#!/usr/bin/python
+#!/usr/bin/python -u
 
 import RPi.GPIO as GPIO, time 
 
-# How often to check the soil moisture? (in seconds)
-poll = 1
+# How often to check the soil moisture when it's dry (the water is on)
+dry_poll = 1 # seconds
 
-# How long should it be dry before we turn on the water? (in seconds)
-alert = 10 
+# How often to check the soil moisture when it's wet (the water is off)
+wet_poll = 15*60 # seconds
 
+# Count like a grown-up
 GPIO.setmode(GPIO.BCM)
+
+# GPIO 17 is our digital sensor input
 GPIO.setup(17, GPIO.IN)
+
+# GPIO 18 is used to power the digital sensor board when polling
+# This is generally a really bad idea, but this is a very low power
+# board and it works well for me anyway.
 GPIO.setup(18, GPIO.OUT)
 
-count = 0
 water = 0
 
 try:
     while True:
         GPIO.output(18, GPIO.HIGH)
-        time.sleep(.1)
+        time.sleep(.1) # Wait for digital sensor to settle
         if GPIO.input(17):
-            print "Dry"
-            count += 1
-        else:
-            print "Wet"
-            count = 0
-        GPIO.output(18, GPIO.LOW)
-        if count > alert / poll:
+            print ','.join((time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), "Dry"))
             if not water:
-                print "Turn on the water!"
+                print ','.join((time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), "Turn on the water!"))
                 water = 1
         else:
+            print ','.join((time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), "Wet"))
             if water:
-                print "Turn off the water!"
+                print ','.join((time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), "Turn off the water!"))
                 water = 0
-        time.sleep(poll)
+        GPIO.output(18, GPIO.LOW)
+        if water:
+            time.sleep(dry_poll)
+        else:
+            time.sleep(wet_poll)
 except KeyboardInterrupt:
     GPIO.cleanup()
